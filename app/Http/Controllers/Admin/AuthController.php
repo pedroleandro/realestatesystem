da3f9b0dd6655b7ddc81361c59237045c44a9e8e
pedro <pedro.leandrog@gmail.com>
2021-11-07 23:51:08 -0300

    Tratando requisição de Login

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -3,12 +3,17 @@
 namespace LaraDev\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use LaraDev\Http\Controllers\Controller;
+use LaraDev\User;
 
 class AuthController extends Controller
 {
     public function showLoginForm()
     {
+//        $user = User::where('id', 1)->first();
+//        $user->password = bcrypt('123456');
+//        $user->save();
         return view('admin.index');
     }
 
@@ -19,9 +24,27 @@ class AuthController extends Controller
 
     public function login(Request $request)
     {
-        if(in_array('', $request->only('email', 'password'))){
-            $json['message'] = 'Whooooops!!! Por favor, informe todos os dados corretamente para efetuar o login.';
+        if (in_array('', $request->only('email', 'password'))) {
+            $json['message'] = $this->message->error('Whooooops!!! Por favor, informe todos os dados corretamente para efetuar o login.')->render();
             return response()->json($json);
         }
+
+        if (!filter_var($request->email, FILTER_VALIDATE_EMAIL)) {
+            $json['message'] = $this->message->error('Whooooops!!! Informe um e-mail válido.')->render();
+            return response()->json($json);
+        }
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+
+        if (!Auth::attempt($credentials)) {
+            $json['message'] = $this->message->error('Whooooops!!! Usuário ou senha inválidos.')->render();
+            return response()->json($json);
+        }
+
+        $json['redirect'] = route('admin.home');
+        return response()->json($json);
     }
 }
